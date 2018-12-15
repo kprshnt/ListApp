09ffecde42370816407869076382c6ca0b72a9b9
Prashant <kr.prashant@gmail.com>
2018-12-15 13:11:02 -0500

    Initial commit
    Initial commit.

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This application allows you to visualize your lists in creative ways.</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
